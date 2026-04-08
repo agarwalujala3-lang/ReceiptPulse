0b254ffb4f40fc1f499a70270634505465362b36
UJALA <agarwalujala3@gmail.com>
2026-04-08 05:50:54 +0530

    Restore live API and Cognito dashboard config

--- dashboard/config.js
+++ dashboard/config.js
@@ -1,12 +1,12 @@
 window.RECEIPTPULSE_CONFIG = {
-  apiBaseUrl: "",
+  apiBaseUrl: "https://xooa7yv1tf.execute-api.ap-south-1.amazonaws.com",
   auth: {
-    hostedUiDomain: "",
-    clientId: "",
-    region: "",
+    hostedUiDomain: "https://receiptpulse-119944160349-20260326.auth.ap-south-1.amazoncognito.com",
+    clientId: "61nlmqvbrs46bc9i4mvsljgf4c",
+    region: "ap-south-1",
     appPath: "./app.html",
-    redirectSignIn: "",
-    redirectSignOut: "",
+    redirectSignIn: "https://d2ijsg7huf2h2p.cloudfront.net",
+    redirectSignOut: "https://d2ijsg7huf2h2p.cloudfront.net",
     scopes: ["openid", "profile"],
   },
 };
